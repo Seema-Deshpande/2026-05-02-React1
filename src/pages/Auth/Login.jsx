477bbcc2c548e97c08e1a0221ca7aa149fbcd497
Seema-Deshpande <seemadeshpande12@gmail.com>
2026-05-02 10:38:17 -0500

    Defined login behavior.

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -7,23 +7,30 @@ function Login() {
   const [ password, setPassword] = useState('')
 
   const handleSubmit = e => {
+    e.preventDefault();
     // Implement logic to print it on console and success on alert
+    console.log("Email:", email);
+    console.log("Password:", password);
+    alert("Login successful!");
   };
 
   return (
     <div className="auth-container">
       <div className="auth-box">
         <h2>Login</h2>
-        <input 
-          value={email}
-          onChange = {(event) => setEmail(event.target.value)}
-          placeholder="Email"
-        />
-        <input 
-          value={password}
-          onChange = {(event) => setPassword(event.target.value)}
-          placeholder='Password'
+        <form onSubmit={handleSubmit}>
+          <input
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            placeholder="Email"
           />
+          <input
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            placeholder='Password'
+          />
+          <button>Login</button>
+        </form>
       </div>
     </div>
   );
