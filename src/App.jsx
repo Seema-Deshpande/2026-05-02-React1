737a4856de69c702e010aba90b929b66733255f8
Seema-Deshpande <seemadeshpande12@gmail.com>
2026-05-02 10:33:17 -0500

    Defined login fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Login from "./pages/Auth/Login";
 import "./App.css"
+import Register from "./pages/Auth/Register";
 
 function App() {
 
   return (
     <div className="app-layout">
+      <Login />
+      <Register />
     </div>
   );
 }
--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -3,6 +3,8 @@ import "./Auth.css";
 
 function Login() {
   // Define relevant state variables for login form
+  const [ email, setEmail] = useState('');
+  const [ password, setPassword] = useState('')
 
   const handleSubmit = e => {
     // Implement logic to print it on console and success on alert
@@ -11,7 +13,17 @@ function Login() {
   return (
     <div className="auth-container">
       <div className="auth-box">
-      {/* Create a Login form */}
+        <h2>Login</h2>
+        <input 
+          value={email}
+          onChange = {(event) => setEmail(event.target.value)}
+          placeholder="Email"
+        />
+        <input 
+          value={password}
+          onChange = {(event) => setPassword(event.target.value)}
+          placeholder='Password'
+          />
       </div>
     </div>
   );
